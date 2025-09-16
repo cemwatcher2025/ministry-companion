47c34fcab3bf427c8cea55e7e6ae983fbc229bb9
cemwatcher2025 <bwoods940@yahoo.com>
2025-09-15 22:28:06 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ministry-companion/",   // 👈 IMPORTANT for GitHub Pages
+  base: "/ministry-companion/",   // 👈 must include leading & trailing slash
   plugins: [react()],
 });
+
